d591495c4bfa2d24a7eca1349da7381d27b2baf2
Akinbami Gbenga <akinbami.gbenga@gmail.com>
2025-09-04 09:58:16 +0100

    Change to port 8080

--- run.py
+++ run.py
@@ -70,7 +70,7 @@ def main():
             sys.executable, "-m", "uvicorn", 
             "main:app", 
             "--host", "0.0.0.0", 
-            "--port", "8001",
+            "--port", "8080",
             "--reload"
         ])
     except KeyboardInterrupt:
